a9e62ed06c401640c71bc53731868e53bcdb27ee
Mayank2@14 <55187056+mayank2021@users.noreply.github.com>
2021-12-28 21:12:35 +0530

    add frontend files

---
+++ console.log/client/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import Nav from "./components/Navbar/Nav";
+import Footer from "./components/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Contact from "./Pages/Contact/Contact";
+import About from "./Pages/About/About";
+import Compose from "./Pages/Compose/Compose";
+import Update from "./Pages/Update/Update";
+import BlogDetail from "./Pages/BlogDetail/BlogDetail";
+
+function App() {
+  return (
+    <>
+    <Nav/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/compose" element={<Compose />} />
+        <Route path="/update" element={<Update />} />     
+        <Route path="/blogs/:id" element={<BlogDetail />} /> 
+      </Routes>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
+
